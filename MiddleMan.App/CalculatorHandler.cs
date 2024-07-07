343ab618f43308a11e105ee71bf604a8f9402f15
Carlos Vigueras <carlosj.vigueras@gmail.com>
2024-07-07 15:58:58 +0200

    feat: implemented calculatorhandle

---
+++ MiddleMan.App/CalculatorHandler.cs
@@ -0,0 +1,31 @@
+﻿using MiddleMan.App.Tests;
+
+namespace MiddleMan.App
+{
+    public class CalculatorHandler
+    {
+        private Calculator Calculator;
+        private Number Number1;
+        private Number Number2;
+
+        public CalculatorHandler()
+        {
+            Calculator = new Calculator();
+        }
+
+        public int Handle()
+        {
+            var random = new Random();
+            Number1 = Number.Create(random.Next(1, 21));
+            Number2 = Number.Create(random.Next(1, 21));
+            if (Number1.Value % 2 == 0)
+            {
+                return Calculator.Sum(Number1, Number2);
+            }
+            else
+            {
+                return Calculator.Split(Number1, Number2);
+            }
+        }
+    }
+}
